08fb108baf5fc5adeaefff4475794256b43a3764
ZouYu <1677290670@qq.com>
2025-10-21 20:32:24 +0800

    Rename some files
    Signed-off-by: ZouYu <1677290670@qq.com>

---
+++ pkg/drivers/apis/apis.go
@@ -0,0 +1,22 @@
+package apis
+
+// Driver interface
+type Driver interface {
+	// Create a new volume with the given name and options.
+	Create(name string, options map[string]string) error
+	// List all volumes.
+	// Returns a map of volume names to their metadata.
+	List() (map[string]*VolumeMetadata, error)
+	// Get retrieves the metadata for a volume by name.
+	Get(name string) (*VolumeMetadata, error)
+	// Remove deletes a volume by name.
+	Remove(name string) error
+	// Path returns the mount point for a volume by name.
+	Path(name string) (string, error)
+	// Mount mounts a volume by name and ID.
+	Mount(name string, id string) (string, error)
+	// Unmount unmounts a volume by name and ID.
+	Unmount(name string, id string) error
+	// Destroy cleans up any resources used by the driver.
+	Destroy() error
+}
--- pkg/drivers/apis/volume_metadata.go
+++ pkg/drivers/apis/volume_metadata.go
@@ -12,27 +12,6 @@ import (
 
 var globalValidator *validator.Validate = validator.New()
 
-// Driver interface
-type Driver interface {
-	// Create a new volume with the given name and options.
-	Create(name string, options map[string]string) error
-	// List all volumes.
-	// Returns a map of volume names to their metadata.
-	List() (map[string]*VolumeMetadata, error)
-	// Get retrieves the metadata for a volume by name.
-	Get(name string) (*VolumeMetadata, error)
-	// Remove deletes a volume by name.
-	Remove(name string) error
-	// Path returns the mount point for a volume by name.
-	Path(name string) (string, error)
-	// Mount mounts a volume by name and ID.
-	Mount(name string, id string) (string, error)
-	// Unmount unmounts a volume by name and ID.
-	Unmount(name string, id string) error
-	// Destroy cleans up any resources used by the driver.
-	Destroy() error
-}
-
 type VolumeMetadata struct {
 	Mountpoint string        `json:"mountpoint" validate:"required"`
 	CreatedAt  time.Time     `json:"createAt" validate:"required"`
